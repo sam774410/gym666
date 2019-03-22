5188d7f938e86df10b7de3c3ca665cd83c616268
Ming-Sheng,Lyu <sam850118sam@gmail.com>
2019-03-22 13:04:24 +0800

    fix

--- app.py
+++ app.py
@@ -186,8 +186,8 @@ def line_single_push(id, txt):
 
 import os
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0',port=os.environ['PORT'])
-    app.run(host='0.0.0.0',port=3000)
+    app.run(host='0.0.0.0',port=os.environ['PORT'])
+    #app.run(host='0.0.0.0',port=3000)
 
 
 
